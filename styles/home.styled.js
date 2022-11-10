441ac5a9d2e212eb0d8264c923b5c9338324c101
Aman Kapoor <60822083+kapaman@users.noreply.github.com>
2022-11-10 12:55:34 +0530

    Created Home/Welcome Screen

--- styles/home.styled.js
+++ styles/home.styled.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Root = styled.div`
-  max-width: 420px;
+  max-width: 400px;
   height: 100%;
   margin: auto;
   border: 1px solid black;
   box-sizing: border-box;
+  position: relative;
 `;
 
 export const GradientBackground = styled.div`
@@ -15,4 +16,31 @@ export const GradientBackground = styled.div`
     #af9cf3 86.48%
   );
   height: 100%;
+`;
+
+export const Logo = styled.div`
+  margin: auto;
+  text-align: center;
+  padding-top: 2rem;
+`;
+
+export const Circle = styled.div`
+  background-color: white;
+  height: 200px;
+  width: 210px;
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  filter: drop-shadow(0px 8px 8px rgba(0, 0, 0, 0.1));
+`;
+
+export const CircleText = styled.p`
+  color: #ff3b3f;
+  font-size: 40px;
+  font-weight: bold;
 `;
