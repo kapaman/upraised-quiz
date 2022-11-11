814cb2ed32dc21b373c51e609f7ba6c378f80ec9
Aman Kapoor <60822083+kapaman@users.noreply.github.com>
2022-11-11 18:52:32 +0530

    Added Question Screen along with styled-components

---
+++ pages/question.js
@@ -0,0 +1,158 @@
+import QuizContext from "../context/QuizProvider";
+import { useContext, useEffect, useState } from "react";
+import PrimaryButton from "../components/PrimaryButton";
+import GreenCheck from "../images/green-check.svg";
+import Image from "next/image";
+import {
+  OptionRoot,
+  OptionsContainer,
+  QuestionRoot,
+} from "../styles/question.styled";
+import ProgressCircle from "../components/ProgressCircle";
+import Router from "next/router";
+import axios from "axios";
+const styles = {
+  checkStyles: {
+    height: "30px",
+    width: "30px",
+  },
+};
+
+export default function Question() {
+  const { questions, userResponse, addUserResponse, quizId } =
+    useContext(QuizContext);
+  const [questionNo, setQuestionNo] = useState(0);
+  const [startTime, setStartTime] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [chosenAnswers, setChosenAnswers] = useState([]);
+
+  useEffect(() => {
+    if (questions == undefined || questions.length == 0) {
+      //questions is undefined means the user has most probably refreshed the page
+      console.log("user reloaded: need to fetch stored data");
+      return;
+    }
+    setStartTime(Date.now());
+    setChosenAnswers(
+      new Array(questions[questionNo].options.length).fill(false)
+    );
+  }, [questionNo, questions]);
+
+  useEffect(() => {
+    setDisabled(!chosenAnswers.some((item) => item));
+  }, [chosenAnswers]);
+
+  const onChange = (position) => {
+    const updatedChosenAnswers = chosenAnswers.map((item, index) =>
+      index === position ? !item : item
+    );
+    setChosenAnswers(updatedChosenAnswers);
+  };
+
+  const onSubmit = async () => {
+    if (disabled) return;
+    const timeTaken = (Date.now() - startTime) / 1000;
+    const chosenAnswersForServer = [];
+    chosenAnswers.forEach((el, i) => {
+      if (el) {
+        chosenAnswersForServer.push(i);
+      }
+    });
+    const newUserResponse = [...userResponse];
+    newUserResponse.push({
+      questionNo,
+      chosenAnswersForServer,
+      timeTaken,
+    });
+    try {
+      await axios.post(`http://localhost:3000/api/${quizId}/questions`, {
+        questionNo,
+        chosenAnswersForServer,
+        timeTaken,
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+    addUserResponse(newUserResponse);
+    if (questionNo == questions.length - 1) {
+      //LAST QUESTION: SEND API REQUENT TO END QUIZ
+      try {
+        await axios.post(`http://localhost:3000/api/${quizId}/endquiz`);
+      } catch (err) {
+        console.log(err.message);
+      }
+      //LAST QUESTION: CHANGE TO REPORT SCREEN
+      Router.push("/report");
+      return;
+    }
+
+    setQuestionNo((ques) => ques + 1);
+  };
+
+  if (questions == undefined || questions.length == 0) {
+    //we reach here if the user refreshes the page on the url /question
+    //in other words the user doesn't follow the required route of going through the welcome page
+    //then the questions page and then here
+    //so we can fetch data on demand from the server to render this page
+    //but since i'm not storing any data in any database i'm just returning a simple p tag
+    return <p>Fetching</p>;
+  }
+  return (
+    <QuestionRoot>
+      <div
+        style={{ position: "absolute", zIndex: "2", left: "50%", top: "7.5%" }}
+      >
+        <ProgressCircle
+          current={questionNo + 1}
+          max={questions.length}
+        ></ProgressCircle>
+      </div>
+      <OptionsContainer>
+        <div
+          style={{
+            overflow: "scrolL",
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.75rem",
+            marginTop: "5rem",
+          }}
+        >
+          <p>
+            {questions[questionNo].question} {questionNo}
+          </p>
+          {questions[questionNo].options.map((ques, index) => (
+            <OptionRoot
+              key={index}
+              check={chosenAnswers[index]}
+              onClick={() => onChange(index)}
+            >
+              {chosenAnswers[index] ? (
+                <Image
+                  style={{ ...styles.checkStyles, marginLeft: "1rem" }}
+                  src={GreenCheck}
+                  alt="green-check"
+                />
+              ) : (
+                <span
+                  style={{
+                    ...styles.checkStyles,
+                    border: "3px solid #DFE0E5",
+                    borderRadius: "50%",
+                    display: "inline-block",
+                    marginLeft: "1rem",
+                  }}
+                ></span>
+              )}
+              <p style={{ marginLeft: "1rem" }}>{ques}</p>
+            </OptionRoot>
+          ))}
+          <PrimaryButton
+            text="Next"
+            onClick={onSubmit}
+            disabled={disabled}
+          ></PrimaryButton>
+        </div>
+      </OptionsContainer>
+    </QuestionRoot>
+  );
+}
---
+++ styles/question.styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const OptionRoot = styled.div`
+  background: #f3f4fa;
+  border-radius: 20px;
+  height: 80px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border: ${({ check }) => (check ? "4px solid #44B77B" : "4px solid #f3f4fa")};
+`;
+
+export const OptionsContainer = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  background: white;
+  flex: 1;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  margin-top: 7rem;
+  overflow: scroll;
+  height: calc(100% - 5rem);
+  padding-bottom: 5rem;
+  position: relative;
+`;
+
+export const QuestionRoot = styled.div`
+  background: #af9cf3;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  height: 100%;
+`;
